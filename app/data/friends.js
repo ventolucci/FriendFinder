4ad235b4380f12c22dfddc0f07047e0f3b5afac1
Denis Rodriguez-Ramos <ventolucci@hotmail.com>
2018-04-20 23:53:41 -0400

    finish homework

---
+++ app/data/friends.js
@@ -0,0 +1,113 @@
+
+
+var friends = [
+  {
+    name: 'Leela',
+    photo: 'http://www.maskerix.com/wp-content/uploads/2017/05/diy-futurama-leela-halloween-costume-idea.jpg',
+    scores: [
+      '1',
+      '2',
+      '3',
+      '4',
+      '5',
+      '5',
+      '4',
+      '3',
+      '2',
+      '1',
+    ]
+  },  {
+    name: 'Peter Griffin',
+    photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrG7zyG9ElGKOsE_ryR7BxcYatKcIt_mXPOHI7tSQU3WUcwgvR',
+    scores: [
+      '2',
+      '3',
+      '4',
+      '5',
+      '5',
+      '4',
+      '3',
+      '2',
+      '1',
+      '2',
+    ]
+  }, {
+    name: 'Vegetta',
+    photo: 'https://coo3.tuvotacion.com/imagenes_opciones/goku-o-vegetta-1-1283602.jpg',
+    scores: [
+      '3',
+      '4',
+      '5',
+      '5',
+      '4',
+      '3',
+      '2',
+      '1',
+      '2',
+      '3',
+    ]
+  },{
+    name: 'Beavis',
+    photo: 'https://hobbydb-production.s3.amazonaws.com/processed_uploads/subject_photo/subject_photo/image/27921/1499294448-26167-9558/beavis-2_large.png',
+    scores: [
+      '4',
+      '5',
+      '4',
+      '3',
+      '2',
+      '1',
+      '2',
+      '3',
+      '4',
+      '5',
+    ]
+  },{
+    name: 'Homer Simpson',
+    photo: 'http://interactive.nydailynews.com/2016/05/simpsons-quiz/img/simp1.jpg',
+    scores: [
+      '1',
+      '4',
+      '2',
+      '5',
+      '4',
+      '3',
+      '2',
+      '4',
+      '5',
+      '1',
+    ]
+  },{
+    name: 'Jessica Rabbit',
+    photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ42OzK0nnp5QQPXeDlyugBXsYV5W-VMIWXuo-NIaM-AaDIF-C-uA',
+    scores: [
+      '4',
+      '5',
+      '4',
+      '5',
+      '3',
+      '2',
+      '3',
+      '4',
+      '5',
+      '1',
+    ]
+  },{
+
+    name: 'Donald Trump',
+    photo: 'https://www.aljazeera.com/mritems/imagecache/mbdxxlarge/mritems/Images/2017/12/9/be94d32d64cf4b68857e6aea46be3912_18.jpg',
+    scores: [
+      '1',
+      '1',
+      '1',
+      '2',
+      '3',
+      '2',
+      '3',
+      '2',
+      '3',
+      '2',
+    ]
+  },
+]
+
+module.exports = friends;
